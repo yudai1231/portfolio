e9c7d45367a3d61871b4fad537413a67c339345f
Kitano Yudai <grandvictorylife@gmail.com>
2020-12-15 23:38:57 +0900

    ページトップ

--- js/main.js
+++ js/main.js
@@ -6,8 +6,24 @@ $(function() {
 		$('.start').fadeOut(500);
 	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
 
+	// ページトップに戻る　初め
+	var pagetop = $('#page_top');   
+	pagetop.hide();
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+			pagetop.fadeIn();
+		} else {
+			pagetop.fadeOut();
+		}
+	});
 	
-
+	pagetop.click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 500); //0.5秒かけてトップへ移動
+		return false;
+	});
+	// ページトップに戻る　終わり
 
 
 
