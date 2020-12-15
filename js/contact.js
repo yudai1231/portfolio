b1a1cc6caebdcc9b69a27d33c1542ab46366b023
Kitano Yudai <grandvictorylife@gmail.com>
2020-12-16 00:20:58 +0900

    contact.js 未完成

---
+++ js/contact.js
@@ -0,0 +1,16 @@
+// let btn = document.querySelector('.buttom');
+// btn.addEventListener('click',function(){
+//    //入力した文字を取得したいので用意した変数（名前編）
+//   let input_name = document.querySelector('#text');
+
+//   // 文字を表示したいので用意した変数
+//   let result_name = document.querySelector('#result_name');
+
+//   if (input_name.value == ''){
+//     result_name.textContent = '名前が空欄です。';
+//     result_name.style.color = 'red';
+//   }else{
+//     result_name.textContent = input_name.value;
+//     result_name.style.color = '';
+//   }
+// });
