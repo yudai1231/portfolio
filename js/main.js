8aad6a1985804e0131b5a43962401d6e78bd9acb
Kitano Yudai <grandvictorylife@gmail.com>
2020-12-18 15:17:53 +0900

    ハンバーガー未完成

--- js/main.js
+++ js/main.js
@@ -77,10 +77,16 @@ $(function() {
 
 	// ハンバーガーメニュー初め
 	$(function () {
-			$('#nav-toggle').on('click', function() {
-			$('body').toggleClass('open');
+		$("#nav-open").on("click", function () {
+		  if ($(this).hasClass("active")) {
+			$(this).removeClass("active");
+			$("#nav-content").removeClass("open").fadeOut(100);
+		  } else {
+			$(this).addClass("active");
+			$("#nav-content").fadeIn(100).addClass("open");
+		  }
 		});
-	});	
+	});
 	// ハンバーガーメニュー終わり
 
 
